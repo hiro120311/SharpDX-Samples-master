296562e0e9747ea0d76f40abd9be62fde6ef69bc
Hiroto Kagemoto <hiro120311@gmail.com>
2022-06-13 22:00:32 +0900

    初回コミット(SRV,CBV同時使用成功)

---
+++ Desktop/Direct3D12/HelloTexture/shaders.hlsl
@@ -0,0 +1,28 @@
+﻿cbuffer ConstantBuffer : register(b0)
+{
+	float4 offset;
+};
+
+struct PSInput
+{
+	float4 position : SV_POSITION;
+	float2 uv : TEXCOORD;
+};
+
+Texture2D g_texture : register(t0);
+SamplerState g_sampler : register(s0);
+
+PSInput VSMain(float4 position : POSITION, float4 uv : TEXCOORD)
+{
+	PSInput result;
+
+	result.position = position + offset;
+	result.uv = uv;
+
+	return result;
+}
+
+float4 PSMain(PSInput input) : SV_TARGET
+{
+	return g_texture.Sample(g_sampler, input.uv);
+}
